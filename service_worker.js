99730fcc9b51dbeb3d68f81bca1cf4b46f19201f
Kacper Pabianiak <kacper.pabianiak1@gmail.com>
2024-05-22 14:09:43 +0200

    Fixed some minor issues

--- service_worker.js
+++ service_worker.js
@@ -2,10 +2,10 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.get("version", function (result) {
     if (result.version) {
       if (result.version < 4) {
-        chrome.tabs.create({ url: "/upgraded.html" });
+        chrome.tabs.create({ url: chrome.runtime.getURL("/upgraded.html") });
       }
     } else {
-      chrome.tabs.create({ url: "/installed.html" });
+      chrome.tabs.create({ url: chrome.runtime.getURL("/installed.html") });
     }
     chrome.storage.local.set({ version: 4 });
   });
@@ -14,18 +14,20 @@ chrome.runtime.onInstalled.addListener(() => {
     if (!result.installed) {
       chrome.tabs.query({}, function (tabs) {
         for (let tab of tabs) {
-          chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ["js/cs.js"]
-          }).then(() => {
-            chrome.tabs.sendMessage(tab.id, {msg: "style", value: "someValue"}, function(response) {
-              if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError.message);
-              } else {
-                console.log("Response:", response);
-              }
-            });
-          }).catch((error) => console.error(`Failed to execute script: ${error}`));
+          if (!tab.url.startsWith("chrome://") && !tab.url.startsWith("chrome-extension://")) {
+            chrome.scripting.executeScript({
+              target: { tabId: tab.id },
+              files: ["js/cs.js"]
+            }).then(() => {
+              chrome.tabs.sendMessage(tab.id, {msg: "style", value: "someValue"}, function(response) {
+                if (chrome.runtime.lastError) {
+                  console.error(chrome.runtime.lastError.message);
+                } else {
+                  console.log("Response:", response);
+                }
+              });
+            }).catch((error) => {});
+          }
         }
       });
       chrome.storage.local.set({ installed: true });
